15fb273b1b9e08f5f057602a556c7d094f52a7e1
Shah Newaz <shahnewaz385@gmail.com>
2019-10-04 11:59:16 +0600

    Assignment-5

---
+++ LibraryManagementWebAPI/LibraryManagementWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementWebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private IStudentService _StudentService;
+
+        public StudentsController(IStudentService StudentService)
+        {
+            _StudentService = StudentService;
+        }
+
+        // GET api/Students/5
+        [HttpGet("{id}")]
+        public List<Student> Get(int id)
+        {
+            return _StudentService.StudentProfile(id);
+        }
+    }
+}
---
+++ LibraryManagementWebAPI/LibraryManagementWebAPI/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementWebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementWebAPI
+{
+    public class StudentService:IStudentService
+    {
+        private IStudentRepository _StudentRepository;
+        public StudentService(IStudentRepository studentRepository)
+        {
+            _StudentRepository = studentRepository;
+        }
+
+        public List<Student> StudentProfile(int Id)
+        {
+            return _StudentRepository.GetStudentInfo(Id);
+        }
+    }
+}
